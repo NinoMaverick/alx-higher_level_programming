59a21fd8252784767c1f1008bd3f6117fa9530c6
NinoMaverick <etengyemmy@gmail.com>
2024-01-12 07:08:55 -0800

    nawa o, python

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+a = 1
+b = 2
 
-if name == "main":
-    """Print the sum of 1 and 2."""
+if __name__ == "__main__":
     from add_0 import add
 
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+
