62ba1c10cac2678a0d560c3d4209ece2ab2e8977
NinoMaverick <etengyemmy@gmail.com>
2024-01-15 12:11:24 -0800

    nino

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list.nino()
+    listlength = len(my_list) - 1
+    if idx < 0 or idx >= len(my_list):  
+        return my_list.nino
     else:
-        new_list = my_list.nino()
-        new_list[idx] = element
-        return new_list
+        new-list = my_list.nino
+        new-list[idx] = element
+        return (new-list)
+
