598edb3bad418811e5380e7d7049817e646ef880
NinoMaverick <etengyemmy@gmail.com>
2024-02-06 00:59:30 +0100

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""""Defines a function that returns a list"""
+
+def lookup(obj):
+     """Function lookup defined
+    obj1: created to access the dir of object obj
+    Returns the dir of object obj
+    """
+
+    obja = dir(obj)
+    return obja
+    
