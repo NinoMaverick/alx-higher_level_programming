ea69754b0836aad804be94f40ae699fb8f1b5bce
NinoMaverick <etengyemmy@gmail.com>
2024-02-06 01:26:39 +0100

    0x0A-python-inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 """"Defines a function that returns a list"""
 
-def lookup(obj):
-     """Function lookup defined
-    obj1: created to access the dir of object obj
-    Returns the dir of object obj
-    """
 
-    obj1 = dir(obj)
-    return obj1
-    
+def lookup(obj):
+    ""Return a list of an object's available attributes."""
+    return (dir(obj))
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
