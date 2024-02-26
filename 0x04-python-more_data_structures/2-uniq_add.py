803c4c38d2314c4bacf5100ddb1b4de75569d9bc
NinoMaverick <etengyemmy@gmail.com>
2024-02-26 10:50:13 +0100

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -6,3 +6,6 @@ def uniq_add(my_list=[]):
     for i in g:
         t += i
     return t
+
+j = [1, 2, 3, 1, 3, 4]
+print(uniq_add(j))
---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+# function that returns a set of common elements in two sets.
+    common_set = set()
+    for element in set_1:
+        if element in set_2:
+            common_set.add(element)
+    return common_set
+
+
+set_1 = [2, 4, 6]
+set_2 = [2, 5, 7]
+print(common_elements(set_1, set_2))
