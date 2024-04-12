aaed1737652d9ef2f6ed4d7f7366f636c7765c70
NinoMaverick <etengyemmy@gmail.com>
2024-04-11 23:11:40 -0700

    0x13-javascript_objects_scopes_closures

--- practiceJS/conditionals.js/ex1.js
+++ practiceJS/conditionals.js/ex1.js
@@ -1,7 +1,8 @@
+#!/usr/bin/node
 let iceCream = "chocolate";
 if (iceCream === "chocolate") {
-    alert("Yay, I love chocolate ice cream")
+    alert("Yay, I love chocolate ice cream");
 }   
 else {
-    alert("Awww, but chocolate is my favorite...")
+    alert("Awww, but chocolate is my favorite...");
 }
