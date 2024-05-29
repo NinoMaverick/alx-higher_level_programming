9eecd6d468bc4165f85f58f217f1fe5849fa8746
NinoMaverick <etengyemmy@gmail.com>
2024-05-29 16:03:55 +0100

    web_jquery

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('#btn_translate').click(function() {
+    $.get(`https://www.fourtonfish.com/hellosalut/hello/?lang=${$('#language_code').val()}`, function(data) {
+      $('#hello').text(data.hello);
+    });
+  });
+});
+
