f399eca9d7108c1b0ce6ea1829c99bfd6b3f53a3
NinoMaverick <etengyemmy@gmail.com>
2024-05-29 05:32:43 -0700

    jquery

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,4 +1,2 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('header').style.color = '#FF0000';
-});
+   document.querySelector('header').style.color = '#FF0000';
 
